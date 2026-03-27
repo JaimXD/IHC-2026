23aed652bee8394773c0fac445aa7b1c11563fe6
Jaml888 <jaimadiel888@gmail.com>
2026-03-26 20:25:18 -0500

    Añadiendo port en db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,8 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'ihc_2026'
+  database: process.env.DB_NAME || 'ihc_2026',
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
